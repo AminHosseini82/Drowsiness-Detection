4b5de46da43f46b10873c75d6e307fb24afbe881
Amin Hosseini <aminhosseini822003@gmail.com>
2025-10-18 22:30:41 +0330

    delete Extra line

--- project.py
+++ project.py
@@ -15,7 +15,6 @@ def calculate_EAR(eye):
 cap = cv2.VideoCapture(0)
 hog_face_detector = dlib.get_frontal_face_detector()
 dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-# dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
     _, frame = cap.read()
