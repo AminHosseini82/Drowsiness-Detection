13ca422cb26ec3100d58155ff4c7effdbe9f3fa3
Amin Hosseini <aminhosseini822003@gmail.com>
2025-10-18 22:19:37 +0330

    start project

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import cv2
 import dlib
-from scipy.spatial import distances
+from scipy.spatial import distance
+
 
 def calculate_EAR(eye):
     A = distance.euclidean(eye[1], eye[5])
@@ -13,7 +14,8 @@ def calculate_EAR(eye):
 
 cap = cv2.VideoCapture(0)
 hog_face_detector = dlib.get_frontal_face_detector()
-dlib_facelandmark = dlib.shape_predictor("F:\Amin_Projects\Learning\Drowsiness-Detection_3\shape_predictor_68_face_landmarks.dat")
+dlib_facelandmark = dlib.shape_predictor(
+    "F:\Amin_Projects\Learning\Drowsiness-Detection_3\shape_predictor_68_face_landmarks.dat")
 
 while True:
     _, frame = cap.read()
@@ -26,39 +28,39 @@ while True:
         leftEye = []
         rightEye = []
 
-        for n in range(36,42):
-        	x = face_landmarks.part(n).x
-        	y = face_landmarks.part(n).y
-        	leftEye.append((x,y))
-        	next_point = n+1
-        	if n == 41:
-        		next_point = 36
-        	x2 = face_landmarks.part(next_point).x
-        	y2 = face_landmarks.part(next_point).y
-        	cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
+        for n in range(36, 42):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            leftEye.append((x, y))
+            next_point = n + 1
+            if n == 41:
+                next_point = 36
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
 
-        for n in range(42,48):
-        	x = face_landmarks.part(n).x
-        	y = face_landmarks.part(n).y
-        	rightEye.append((x,y))
-        	next_point = n+1
-        	if n == 47:
-        		next_point = 42
-        	x2 = face_landmarks.part(next_point).x
-        	y2 = face_landmarks.part(next_point).y
-        	cv2.line(frame,(x,y),(x2,y2),(0,255,0),1)
+        for n in range(42, 48):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            rightEye.append((x, y))
+            next_point = n + 1
+            if n == 47:
+                next_point = 42
+            x2 = face_landmarks.part(next_point).x
+            y2 = face_landmarks.part(next_point).y
+            cv2.line(frame, (x, y), (x2, y2), (0, 255, 0), 1)
 
         left_ear = calculate_EAR(leftEye)
         right_ear = calculate_EAR(rightEye)
 
-        EAR = (left_ear+right_ear)/2
-        EAR = round(EAR,2)
-        if EAR<0.26:
-        	cv2.putText(frame,"DROWSY",(20,100),
-        		cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),4)
-        	cv2.putText(frame,"Are you Sleepy?",(20,400),
-        		cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),4)
-        	print("Drowsy")
+        EAR = (left_ear + right_ear) / 2
+        EAR = round(EAR, 2)
+        if EAR < 0.26:
+            cv2.putText(frame, "DROWSY", (20, 100),
+                        cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 4)
+            cv2.putText(frame, "Are you Sleepy?", (20, 400),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+            print("Drowsy")
         print(EAR)
 
     cv2.imshow("Are you Sleepy", frame)
@@ -69,10 +71,7 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-
-
-
-#------------------------------------------------------------------------- just testing------------------------------------------------------------
+# ------------------------------------------------------------------------- just testing------------------------------------------------------------
 # cap = cv2.VideoCapture(0)
 # hog_face_detector = dlib.get_frontal_face_detector()
 # dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
